b3f5288fc77731c28b275a2e5a0c3bbb420fe065
Mitali2700 <201B155@juetguna.in>
2022-07-22 21:36:52 +0530

    Synchronous API - fs.copyFileSync

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 //jshint eversion:6
 
 //  The Node REPL(Read Evaluation Print Loop)
-import fs from "fs";
+//import fs from "fs";
+
+const fs=require("fs");
+
+fs.copyFileSync("file1.txt","file2.txt");
+
+//     When we writr file1- I am file.
+//    file2 - I am file2.txt .
+// When we save it and run ->   node index.js      in hyper so file2 text will replaced by file1 text.
