0e36a0c777170dca3c61a83c31b90712081d6a9c
Mitali2700 <201B155@juetguna.in>
2022-07-23 19:51:35 +0530

    Adding dependencies

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
+/*
 //jshint eversion:6
 
 //  The Node REPL(Read Evaluation Print Loop)
 //import fs from "fs";
 
-const fs=require("fs");
+//const fs=require("fs");
 
-fs.copyFileSync("file1.txt","file2.txt");
+//fs.copyFileSync("file1.txt","file2.txt");
 
 //     When we writr file1- I am file.
 //    file2 - I am file2.txt .
 // When we save it and run ->   node index.js      in hyper so file2 text will replaced by file1 text.
+
+*/
+
+/* 
+npm init
+then ,   some of them comes  by default and rest of them fill by yourself
+version: (1.0.0)        
+description: This is a intro to node project   // Write yourself                                 
+entry point: (index.js)                                                           
+test command:                                                                     
+git repository: (https://github.com/Mitali2700/Introduction_Node.js.git)          
+keywords:                                                                         
+author: Mitali Gupta     // Write yourself
+
+npmjs.com-  choose package as per your choice 
+write:-   npm install superheroes
+then you will get dependencies inside package.json
+
+
+*/
