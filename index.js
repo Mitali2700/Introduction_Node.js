ff59c279931fbae25215104e2aa770a18ead5b6e
Mitali2700 <201B155@juetguna.in>
2022-07-23 23:51:09 +0530

    Adding supervillains package

--- index.js
+++ index.js
@@ -34,7 +34,38 @@ then you will get dependencies inside package.json
 
 */
 
+/*
+********************* superheroes package***********************
+
 var superheroes = require("superheroes");
 //import { random } from "superheroes";
 var mySuperheroName= superheroes.random();
 console.log(mySuperheroName);
+*/
+
+/********************* supervillains package***********************/
+const supervillains = require('supervillains');
+ 
+supervillains.random();
+
+
+/* 
+Github Readme
+<h1 align="center">Introduction to Node.js</h1>
+
+
+<p align="center">
+fs.copyFileSync("file1.txt","file2.txt");
+<br>When we write <br>
+file1- I am file.<br>
+file2 - I am file2.txt .<br>
+When we save it and run ->   node index.js in hyper so file2 text will replaced by file1 text.
+</p>
+<br>
+<hr>
+
+Example: Using superhero package<br>
+var superheroes = require("superheroes");<br>
+var mySuperheroName= superheroes.random();<br>
+console.log(mySuperheroName);
+ */
